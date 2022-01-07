e4da438b881a06565aa3c20970b7fd7d28329a9f
Stefan Eischet <stefan@eischet.net>
2022-01-07 13:50:54 +0100

    apply MIT license to new files, read back Content-Encoding as the Apache client does

--- ews-client-java/src/main/java/com/eischet/ews/javaclient/JavaClient.java
+++ ews-client-java/src/main/java/com/eischet/ews/javaclient/JavaClient.java
@@ -1,3 +1,26 @@
+/*
+ * The MIT License
+ * Copyright (c) 2022 Eischet Software e.K.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
 package com.eischet.ews.javaclient;
 
 import com.eischet.ews.api.core.exception.http.EWSHttpException;
@@ -13,7 +36,6 @@ import java.nio.charset.StandardCharsets;
 import java.time.Duration;
 import java.time.temporal.ChronoUnit;
 import java.util.Base64;
-import java.util.Objects;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -122,13 +144,12 @@ public class JavaClient implements ExchangeHttpClient {
                 final HttpRequest request = builder.build();
                 response = buildClient().send(request, HttpResponse.BodyHandlers.ofString());
                 setResponseCode(response.statusCode());
-                setResponseContentType(response.headers().firstValue("content-type").orElse(null));
+                setResponseContentType(response.headers().firstValue("Content-Type").orElse(null));
                 setResponseText(response.body());
-
+                setContentEncoding(response.headers().firstValue("Content-Encoding").orElse(null));
                 if (cookies != null) {
                     cookies.addAll(response.headers().allValues("Set-Cookie"));
                 }
-
                 return response.statusCode();
             } catch (URISyntaxException e) {
                 throw new EWSHttpException("invalid request URI: " + getUrl(), e);
